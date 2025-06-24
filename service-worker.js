535fde900821ff5ea1ebfd6a7ad5e27302ae45e3
Ethan561621 <baguiogonzaless@gmail.com>
2025-06-24 09:36:10 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,35 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("static").then((cache) =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./icon.png",
-        "./manifest.json"
-      ])
-    )
+const CACHE_NAME = "love-you-cache-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "manifest.json",
+  "icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
